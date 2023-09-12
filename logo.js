bfd3f29e523ea6eb9a76d4cf035b72779046f74a
lheumell <56625155+lheumell@users.noreply.github.com>
2023-09-12 11:38:31 +0200

    Update logo.js

--- logo.js
+++ logo.js
@@ -1,16 +1,11 @@
-import LogoImage from './Logo.png'; // Assurez-vous que le chemin est correct
+function createLogo(context) {
+  // you can also use an image (even svg) here. You can even animate your logo.
 
-export default function createLogo(context) {
-  // Créez l'élément img
-  const logo = document.createElement('img');
-
-  // Définissez la source de l'image sur l'URL importée
-  logo.src = LogoImage;
-
-  // Définissez d'autres styles ou attributs au besoin
-  logo.style.margin = '5px';
-  logo.style.width = '100px'; // Par exemple, définissez la largeur de l'image
-  logo.alt = 'Your Logo'; // Ajoutez un attribut alt pour l'accessibilité
-
+  const logo = document.createElement("span");
+  logo.appendChild(document.createTextNode("Your Logo"));
+  logo.style.margin = "5px";
+  logo.style.fontSize = "32px";
+  logo.style.color = "#aaaaaa";
+  
   return logo;
 }
